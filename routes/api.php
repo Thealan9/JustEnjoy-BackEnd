7083b5881dcc3229c20e8692c1d282ae230afb25
Alan <removilak3000@gmail.com>
2025-12-19 14:04:43 -0600

    configuracion inicial rutas login register user

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+Route::middleware('auth:sanctum')->get('/yo', function (Request $request) {
+    return $request->user();
+});
+
